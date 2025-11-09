f248c362b8d2711da9e54326fc35362c16ce58bd
Sam Hyde <mc.flinch.faith@gmail.com>
2025-11-09 08:52:49 -0600

    We in the car, we drunk as shit
    Told my brother "slow down" because we swervin like a bitch, yahhhh
    
    - Fix/add tooltips (?)

--- appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtHoe.java
+++ appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtHoe.java
@@ -1,5 +1,13 @@
 package com.github.hummel.dirtequipment.item;
 
+import java.util.List;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import com.github.hummel.dirtequipment.init.Materials;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemHoe;
