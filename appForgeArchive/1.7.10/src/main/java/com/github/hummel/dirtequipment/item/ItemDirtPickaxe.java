4de90eb357d0d715aa57553e014b3494d7ea4b05
Sam Hyde <mc.flinch.faith@gmail.com>
2025-11-10 02:11:15 -0600

    Bitch I- Bitch I go, yeah I miss my brother that's fashooo
    I hit the store before I would hit your ho
    
    - Fix tooltips (?) -- new attempt -- #12
    (for real this time)

--- appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtPickaxe.java
+++ appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtPickaxe.java
@@ -21,12 +21,16 @@ public class ItemDirtPickaxe extends ItemPickaxe {
         setTextureName("dirtequipment:dirt_pickaxe");
     }
 
-@Override
-@SideOnly(Side.CLIENT)
-public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean advanced) {
-    super.addInformation(stack, player, tooltip, advanced);
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
 
-    tooltip.add("");
+        tooltip.add("");
+        tooltip.add(EnumChatFormatting.BLUE + "+0 Attack Damage" + EnumChatFormatting.RESET + "\u00A7r");
 
-    tooltip.add(EnumChatFormatting.BLUE + "+0 Attack Damage" + EnumChatFormatting.RESET + "\u00A7r");
+        while (!tooltip.isEmpty() && tooltip.get(tooltip.size() - 1).toString().trim().isEmpty()) {
+            tooltip.remove(tooltip.size() - 1);
+        }
+    }
 }
