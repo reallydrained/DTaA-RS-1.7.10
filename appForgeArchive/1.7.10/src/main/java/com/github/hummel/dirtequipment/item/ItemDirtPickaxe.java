cd4d11a487aa39aa2ef176912d6d87980de9ee7a
Sam Hyde <mc.flinch.faith@gmail.com>
2025-11-10 05:20:35 -0600

    I'm rockin Birkenstocks
    I'm like a chill ass muhhfucka
    
    - Simplify tooltip.add code
    - Fix mcmod.info
    - Add correct logo

--- appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtPickaxe.java
+++ appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtPickaxe.java
@@ -24,13 +24,7 @@ public class ItemDirtPickaxe extends ItemPickaxe {
     @Override
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean advanced) {
-        super.addInformation(stack, player, tooltip, advanced);
-
         tooltip.add("");
-        tooltip.add(EnumChatFormatting.BLUE + "+0 Attack Damage" + EnumChatFormatting.RESET + "\u00A7r");
-
-        while (!tooltip.isEmpty() && tooltip.get(tooltip.size() - 1).toString().trim().isEmpty()) {
-            tooltip.remove(tooltip.size() - 1);
-        }
+        tooltip.add("\u00A79+0 Attack Damage");
     }
 }
